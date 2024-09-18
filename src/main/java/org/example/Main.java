e633305c99588d54e8c9babff031633f899629bd
mynor hidalgo <stmhida6@upana.edu.gt>
2024-09-18 12:13:37 -0600

    se mejoro para insertar la salida a la base de datos mySQL

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,51 @@
 package org.example;
+import java.io.IOException;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        BufferCompartido buffer = new BufferCompartido(10);
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+
+        Thread productor1 = new Thread(new Productor(buffer, "input1.txt", "Productor 1"));
+        Thread productor2 = new Thread(new Productor(buffer, "input2.txt", "Productor 2"));
+        Thread productor3 = new Thread(new Productor(buffer, "input3.txt", "Productor 3"));
+        productor1.start();
+        productor2.start();
+        productor3.start();
+
+        Thread consumidor1 = null;
+        Thread consumidor2 = null;
+
+        try {
+            consumidor1 = new Thread(new Consumidor(buffer,  "org.example.Consumidor 1"));
+            consumidor2 = new Thread(new Consumidor(buffer,  "org.example.Consumidor 2"));
+        } catch (IOException e) {
+            e.printStackTrace();
         }
+
+        if (consumidor1 != null) {
+            consumidor1.start();
+        }
+        if (consumidor2 != null) {
+            consumidor2.start();
+        }
+
+        try {
+            productor1.join();
+            productor2.join();
+            productor3.join();
+            if (consumidor1 != null) {
+                consumidor1.join();
+            }
+            if (consumidor2 != null) {
+                consumidor2.join();
+            }
+
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
+
     }
 }
